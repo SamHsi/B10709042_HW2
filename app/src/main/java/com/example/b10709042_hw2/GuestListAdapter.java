840557c9ac384dd36a761fa31a15d88be988750a
l3339 <l3339911@gmail.com>
2020-05-26 03:57:58 +0800

    Done

--- app/src/main/java/com/example/b10709042_hw2/GuestListAdapter.java
+++ app/src/main/java/com/example/b10709042_hw2/GuestListAdapter.java
@@ -23,77 +23,45 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
 
     private Context mContext;
     private int color;
-    // Replace the mCount with a new Cursor field called mCursor
-    // Holds on to the cursor to display the waitlist
     private Cursor mCursor;
 
-    // COMPLETED (2) Modify the constructor to accept a cursor rather than an integer
-    /**
-     * Constructor using the context and the db cursor
-     * @param context the calling context/activity
-     * @param cursor the db cursor with waitlist data to display
-     */
     public GuestListAdapter(Context context, Cursor cursor) {
         this.mContext = context;
-        // Set the local mCount to be equal to count
         this.mCursor = cursor;
     }
 
     @Override
     public GuestViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        // Get the RecyclerView item layout
         LayoutInflater inflater = LayoutInflater.from(mContext);
         View view = inflater.inflate(R.layout.guest_list_item, parent, false);
+        setupColor();
         return new GuestViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(GuestViewHolder holder, int position) {
-        // COMPLETED (5) Move the cursor to the passed in position, return if moveToPosition returns false
-        // Move the mCursor to the position of the item to be displayed
         if (!mCursor.moveToPosition(position))
-            return; // bail if returned null
-        // COMPLETED (6) Call getString on the cursor to get the guest's name
+            return;
         String name = mCursor.getString(mCursor.getColumnIndex(WaitlistContract.WaitlistEntry.COLUMN_GUEST_NAME));
-        // COMPLETED (7) Call getInt on the cursor to get the party size
         int partySize = mCursor.getInt(mCursor.getColumnIndex(WaitlistContract.WaitlistEntry.COLUMN_PARTY_SIZE));
         long id = mCursor.getLong(mCursor.getColumnIndex(WaitlistContract.WaitlistEntry._ID));
-        // COMPLETED (8) Set the holder's nameTextView text to the guest's name
-        // Display the guest name
         setupColor();
         holder.nameTextView.setText(name);
-        // COMPLETED (9) Set the holder's partySizeTextView text to the party size
-        // Display the party count
         holder.partySizeTextView.setText(String.valueOf(partySize));
         holder.partySizeTextView.getBackground().setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
-
-
         holder.itemView.setTag(id);
 
     }
 
-    // Modify the getItemCount to return the mCount value rather than 0
     @Override
     public int getItemCount() {
         return mCursor.getCount();
     }
 
-    // COMPLETED (15) Create a new function called swapCursor that takes the new cursor and returns void
-    /**
-     * Swaps the Cursor currently held in the adapter with a new one
-     * and triggers a UI refresh
-     *
-     * @param newCursor the new cursor that will replace the existing one
-     */
     public void swapCursor(Cursor newCursor) {
-        // COMPLETED (16) Inside, check if the current cursor is not null, and close it if so
-        // Always close the previous mCursor first
         if (mCursor != null) mCursor.close();
-        // COMPLETED (17) Update the local mCursor to be equal to  newCursor
         mCursor = newCursor;
-        // COMPLETED (18) Check if the newCursor is not null, and call this.notifyDataSetChanged() if so
         if (newCursor != null) {
-            // Force the RecyclerView to refresh
             this.notifyDataSetChanged();
         }
     }
@@ -117,24 +85,10 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
         }
     }
 
-
-    /**
-     * Inner class to hold the views needed to display a single item in the recycler-view
-     */
     class GuestViewHolder extends RecyclerView.ViewHolder {
-
-        // Will display the guest name
         TextView nameTextView;
-        // Will display the party size number
         TextView partySizeTextView;
 
-        /**
-         * Constructor for our ViewHolder. Within this constructor, we get a reference to our
-         * TextViews
-         *
-         * @param itemView The View that you inflated in
-         *                 {@link GuestListAdapter#onCreateViewHolder(ViewGroup, int)}
-         */
         public GuestViewHolder(View itemView) {
             super(itemView);
             nameTextView = (TextView) itemView.findViewById(R.id.name_text_view);
--- app/src/main/java/com/example/b10709042_hw2/MainActivity.java
+++ app/src/main/java/com/example/b10709042_hw2/MainActivity.java
@@ -28,68 +28,36 @@ import com.example.b10709042_hw2.data.WaitlistDBHelper;
 public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
     private final static String LOG_TAG = MainActivity.class.getSimpleName();
     private GuestListAdapter mAdapter;
-    // Create a local field SQLiteDatabase called mDb
     private SQLiteDatabase mDb;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         RecyclerView waitlistRecyclerView;
-
-        // Set local attributes to corresponding views
         waitlistRecyclerView = (RecyclerView) this.findViewById(R.id.all_guests_list_view);
-
-        // Set layout for the RecyclerView, because it's a list we are using the linear layout
         waitlistRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        // Create a WaitlistDbHelper instance, pass "this" to the constructor
-        // Create a DB helper (this will create the DB if run for the first time)
         WaitlistDBHelper dbHelper = new WaitlistDBHelper(this);
-
-        // Get a writable database reference using getWritableDatabase and store it in mDb
-        // Keep a reference to the mDb until paused or killed. Get a writable database
-        // because you will be adding restaurant customers
         mDb = dbHelper.getWritableDatabase();
-
-        // Run the getAllGuests function and store the result in a Cursor variable
         Cursor cursor = getAllGuests();
-
-        // Pass the entire cursor to the adapter rather than just the count
-        // Create an adapter for that cursor to display the data
         mAdapter = new GuestListAdapter(this, cursor);
-
-        // Link the adapter to the RecyclerView
         waitlistRecyclerView.setAdapter(mAdapter);
-        setupSharedPreferences();
 
-        // COMPLETED (3) Create a new ItemTouchHelper with a SimpleCallback that handles both LEFT and RIGHT swipe directions
-        // Create an item touch helper to handle swiping items off the list
         new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
-            // COMPLETED (4) Override onMove and simply return false inside
             @Override
             public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
-                //do nothing, we only care about swiping
                 return false;
             }
-
-            // COMPLETED (5) Override onSwiped
             @Override
             public void onSwiped(final RecyclerView.ViewHolder viewHolder, int swipeDir) {
                 new AlertDialog.Builder(MainActivity.this).setTitle(R.string.warning).setMessage(R.string.warningcheck)
                         .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        // COMPLETED (8) Inside, get the viewHolder's itemView's tag and store in a long variable id
-                        //get the id of the item being swiped
                         long id = (long) viewHolder.itemView.getTag();
                         Log.d(LOG_TAG, String.valueOf(id));
-                        // COMPLETED (9) call removeGuest and pass through that id
-                        //remove from DB
                         removeGuest(id);
-                        // COMPLETED (10) call swapCursor on mAdapter passing in getAllGuests() as the argument
-                        //update the list
                         mAdapter.swapCursor(getAllGuests());
                     }
                 }).setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
@@ -99,8 +67,13 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
                     }
                 }).create().show();
             }
-            //COMPLETED (11) attach the ItemTouchHelper to the waitlistRecyclerView
         }).attachToRecyclerView(waitlistRecyclerView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
     }
 
     @Override
@@ -114,14 +87,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         sharedPreferences.registerOnSharedPreferenceChangeListener(this);
     }
 
-    // Create a private method called getAllGuests that returns a cursor
-    /**
-     * Query the mDb and get all guests from the waitlist table
-     *
-     * @return Cursor containing the list of guests
-     */
     private Cursor getAllGuests() {
-        // Inside, call query on mDb passing in the table name and projection String [] order by COLUMN_TIMESTAMP
         return mDb.query(
                 WaitlistContract.WaitlistEntry.TABLE_NAME,
                 null,
@@ -156,15 +122,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         return super.onOptionsItemSelected(item);
     }
 
-    // COMPLETED (1) Create a new function called removeGuest that takes long id as input and returns a boolean
-    /**
-     * Removes the record with the specified id
-     *
-     * @param id the DB id to be removed
-     * @return True: if removed successfully, False: if failed
-     */
     private boolean removeGuest(long id) {
-        // COMPLETED (2) Inside, call mDb.delete to pass in the TABLE_NAME and the condition that WaitlistEntry._ID equals id
         return mDb.delete(WaitlistContract.WaitlistEntry.TABLE_NAME, WaitlistContract.WaitlistEntry._ID + "=" + id, null) > 0;
     }
 
